3c71cabe29696a3ba3d3bc1615fa90acbbb27254
Logyser <seguimientologyser@gmail.com>
2025-10-12 14:55:35 -0500

    Actualizar app.js
    Se actualiza nuevamente el archivo

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
-const cors = require('cors');
-app.use(cors());
 const express = require('express');
+const cors = require('cors');           // <-- Primero require express y luego cors
 const bodyParser = require('body-parser');
 const actasController = require('./controllers/actasController');
-const app = express();
+const app = express();                  // <-- Crea el objeto app aquí
 
+app.use(cors());                        // <-- Ahora esto funciona correctamente
 app.use(bodyParser.json({ limit: '5mb' })); // para aceptar imágenes grandes en base64
 app.use('/api/actas', actasController);
 
