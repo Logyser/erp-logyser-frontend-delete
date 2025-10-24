1a2c97f49a6696e3c968f0cc1bb0ef90983d6efa
Logyser <seguimientologyser@gmail.com>
2025-10-24 08:45:17 -0500

    mas cambios
    probar

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const cors = require('cors');           // <-- Primero require express y luego cors
+const cors = require('cors');
 const bodyParser = require('body-parser');
+
 const actasController = require('./controllers/actasController');
 const encuestasController = require('./controllers/encuestasController');
-const app = express();                  // <-- Crea el objeto app aquí
 
-app.use(cors());                        // <-- Ahora esto funciona correctamente
+const app = express();
+
+app.use(cors());
 app.use(bodyParser.json({ limit: '5mb' })); // para aceptar imágenes grandes en base64
 app.use('/api/actas', actasController);
 app.use('/api/encuestas', encuestasController);
